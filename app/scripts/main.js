d8e794ca493de3ec3f6d17acf1e76f5567237fd7
Roland <rdorde@gmail.com>
2017-05-01 01:13:11 +0200

    chore: add hunt.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,3 +5,7 @@ function toggleOpen() {
 }
 
 ad.addEventListener('click', toggleOpen);
+
+
+// flex: 5 when user scrolls to the ad
+// collapse to just heading and after that animation ends change back to flex: 1 when the used closes the panel
